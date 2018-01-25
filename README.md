fa3de779c3243cca4f7a8eab1060688cb4fe35ca
Tianshu Huang <thetianshuhuang@gmail.com>
2018-01-25 09:03:05 -0600

    Mostly finished with server code. Client code should be set up as a finite state machine like the server to make modification easier.

--- README.md
+++ README.md
@@ -1,28 +1,28 @@
 # lab1
-Lab 1: Secure HMAC Button
+Lab 1: Secure Button using challenge-response authentication and SHA-256 HMAC.
 
-## Dependencies:
+## Dependencies
 - Crypto Library at https://rweather.github.io/arduinolibs/crypto.html
 - RF24
 
-## Communication protocol:
+## Communication protocol
+The entire protocol restarts if any communication times out.
 
 ### Client
-1. Send request (0xF...F)
-2. Listen for request (timeout = 250ms; goto 1)
-3. Get challenge
-4. Compute response = H(challenge XOR H(key))
+1. Send request (0xFF)
+2. Listen for request (timeout = 250ms)
+3. Get challenge (char[32] or byte[32]); reject if challenge == 0x0...0 or 0xF...F
+4. Compute response = HMAC(challenge, password)
 5. Send response
-6. Wait for response (timeout = 250ms; goto 1).
-7. If response is pass (0xF...F), wait for 500ms. Else, goto 1 without waiting.
-8. Goto 1
+6. Wait for response (timeout = 250ms)
+7. If response is pass (0xFF), wait for 500ms, then restart. If the response is fail (0x00), restart immediately.
 
 ### Server
-1. Listen for request (0xF...F)
-2. Generate challenge on request receipt.
+1. Listen for request (0xFF)
+2. Generate challenge (char[32]) on request receipt.
 3. Send challenge.
-4. Listen for response (timeout = 250ms; goto 1)
-5. Check: response = H(challenge XOR H(password))?
-	- If no, send (0x0...0); goto 1
-6. Send pass (0xF...F)
+4. Listen for response (timeout = 250ms)
+5. Check: response = HMAC(challenge, password)?
+	- If no, send fail (0x00); go back to beginning
+6. Send pass (0xFF)
 7. Turn on the light if not already on; set the new expiration time to +1s
---
+++ server.cpp
@@ -0,0 +1,148 @@
+// default arduino library
+#include <Arduino.h>
+
+// crypto library
+#include <SHA256.h>
+#include <RNG.h>
+#include <TransistorNoiseSource.h>
+
+// RF library
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+// Radio
+RF24 radio(3, 2);
+const byte address[6] = "00001";
+const char pass = 0xFF;
+const char fail = 0x00;
+
+// Crypto
+TransistorNoiseSource noise(A1);
+sha256 SHA256();
+const char* key = "super secret key"
+
+int main() {
+
+  // Radio
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+  char responsecode;
+  byte challenge[32];
+  byte response[32];
+  byte hmac[32];
+
+  // Crypto
+  RNG.begin("MyApp 1.0", 500);
+  RNG.addNoiseSource(noise);
+    
+  // Control
+  int state = 1; // implemented as a finite-state machine
+  long timeout_time;
+  long light_expire_time = 0;
+
+  while(true) {
+
+      // check for message
+      if(state == 1) {
+        if(radio.available()) {
+          radio.read(&responsecode, sizeof(responsecode));
+          if(responsecode == pass) {
+            state = 2;
+          }
+        }
+      }
+
+      // generate challenge
+      else if(state == 2) {
+        RNG.rand(challenge, sizeof(challenge));
+        state = 3;
+      }
+
+      // send challenge
+      else if(state == 3) {
+          // write challenge
+          radio.openWritingPipe(address);
+          radio.stopListening();
+          radio.write(&challenge, sizeof(challenge));
+
+          // reopen read pipe
+          radio.openReadingPipe(0, address);
+          radio.startListening();
+
+          state = 4;
+          // compute timeout time
+          long timeout_time = millis() + 250;
+      }
+
+      // listen for response
+      else if(state == 4) {
+        if(millis() < timeout_time) {
+          // get response
+          if(radio.available()) {
+            radio.read(&response, sizeof(response));
+            state = 5;
+          }
+        }
+        // timeout
+        else {
+          state = 1;
+        }
+      }
+
+      // verify response
+      else if(state == 5) {
+        hash.resetHMAC(key, sizeof(key));
+        hash.update(challenge, sizeof(challenge));
+        hash.finalizeHMAC(key, sizeof(key), hmac, sizeof(hmac));
+
+        bool pass = true;
+        for(int i=0; i<32; i++) {
+          if(hmac[i] != response[i]) {
+            pass = false;
+          }
+        }
+
+        if(pass) {
+          state == 6;
+        }
+        else {
+          state == -6;
+        }
+      }
+
+      // send pass
+      else if(state == 6) {
+        radio.openWritingPipe(address);
+        radio.stopListening();
+        radio.write(&pass, sizeof(pass));
+        state = 7;
+      }
+
+      // send fail
+      else if(state == -6) {
+        radio.openWritingPipe(address);
+        radio.stopListening();
+        radio.write(&fail, sizeof(fail));
+        state = 1;        
+      }
+
+      // turn on light
+      else if(state == 7) {
+        light_expire_time = millis() + 1000;
+        radio.openReadingPipe(0, address);
+        radio.startListening();        
+        state = 1;
+      }
+
+    if(millis() < light_expire_time) {
+      // todo: turn the light on
+    }
+
+    // stir the RNG
+    RNG.loop();
+
+  }
+}
