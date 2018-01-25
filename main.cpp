8762e883a7cd28a23ea385be47ff698c3e7ddd90
Tianshu Huang <thetianshuhuang@gmail.com>
2018-01-24 20:25:20 -0600

    Pasted in template.

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <Crypto.h>
+#include <RNG.h>
+#include <TransistorNoiseSource.h>
+#include <Arduino.h>
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+// Radio
+RF24 radio(3, 2);
+const byte address[6] = "00001";
+
+// Crypto
+TransistorNoiseSource noise(A1);
+const char * key = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+
+int main() {
+  // Radio
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+
+  // Crypto
+  RNG.begin("MyApp 1.0", 500);
+
+  while(True) {
+      byte challenge[32];
+
+      RNG.addNoiseSource(noise);
+    
+      RNG.rand(challenge, sizeof(challenge));
+    
+      if (radio.available()) {
+        char text[32];
+        radio.read(&text, sizeof(text));
+        Serial.println(text);
+      }
+  }
+}
